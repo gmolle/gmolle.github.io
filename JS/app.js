89ac3fa7b56d0f181ee5bb30ab2f3e7d26ee1c5a
gmolle <46017064+gmolle@users.noreply.github.com>
2019-03-02 01:58:35 -0500

    Reduced repeated code in event listeners

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,9 @@
 const button1 = document.querySelector('.section1');
 const button2 = document.querySelector('.section2');
 const button3 = document.querySelector('.backTo1');
-const project1 = document.querySelector('.toDoList');
-const project2 = document.querySelector('.toDoList2');
-const project3 = document.querySelector('.toDoList3');
-const project4 = document.querySelector('.toDoList4');
 const modalBG = document.querySelector('.bg-modal');
 const close = document.querySelector('.close');
+const projects = document.querySelectorAll('.toDoList');
 //////////////////////////////////////////
 
 // Functions
@@ -43,69 +40,30 @@ button3.addEventListener('click', () => {
 	window.scrollTo(0, 0);
 });
 
-project1.addEventListener ('mouseover', () => {
-	project1.style.transform = 'scale(1.02)';
-	project1.style.transition = '.5s';
-});
+for (let i = 0; i < projects.length; i++) {
+	projects[i].addEventListener('mouseover', () => {
+		projects[i].style.transform = 'scale(1.02)';
+		projects[i].style.transition = '.5s';
+	});
+}
 
-project1.addEventListener ('mouseout', () => {
-	project1.style.transform = 'scale(1)';
-	project1.style.transition = '.5s';
-});
-
-project2.addEventListener ('mouseover', () => {
-	project2.style.transform = 'scale(1.02)';
-	project2.style.transition = '.5s';
-});
-
-project2.addEventListener ('mouseout', () => {
-	project2.style.transform = 'scale(1)';
-	project2.style.transition = '.5s';
-});
-
-project3.addEventListener ('mouseover', () => {
-	project3.style.transform = 'scale(1.02)';
-	project3.style.transition = '.5s';
-});
-
-project3.addEventListener ('mouseout', () => {
-	project3.style.transform = 'scale(1)';
-	project3.style.transition = '.5s';
-});
-
-project4.addEventListener ('mouseover', () => {
-	project4.style.transform = 'scale(1.02)';
-	project4.style.transition = '.5s';
-});
-
-project4.addEventListener ('mouseout', () => {
-	project4.style.transform = 'scale(1)';
-	project4.style.transition = '.5s';
-});
+for (let i = 0; i < projects.length; i++) {
+	projects[i].addEventListener('mouseout', () => {
+		projects[i].style.transform = 'scale(1)';
+		projects[i].style.transition = '.5s';
+	});
+}
 
 // Modal Open & Close
-project1.addEventListener('click', () => {
-	modalBG.style.display = 'flex';
-});
-
-project2.addEventListener('click', () => {
-	modalBG.style.display = 'flex';
-});
-
-project3.addEventListener('click', () => {
-	modalBG.style.display = 'flex';
-});
-
-project4.addEventListener('click', () => {
-	modalBG.style.display = 'flex';
-});
-
+for (let i = 0; i < projects.length; i++) {
+	projects[i].addEventListener('click', () => {
+		modalBG.style.display = 'flex';
+	});
+}
 
 close.addEventListener('click', () => {
 	modalBG.style.display = 'none';
 });
-
-
 //////////////////////////////////////////
 
 
