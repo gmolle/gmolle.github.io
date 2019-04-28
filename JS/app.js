edaa4dc809ebdea63e05c2e3fd44e851a0a58431
gmolle <46017064+gmolle@users.noreply.github.com>
2019-04-28 01:54:05 -0400

    Added Bootstrap
    Added navbar and modal's from bootstrap for better functionality

--- JS/app.js
+++ JS/app.js
@@ -15,39 +15,18 @@ const projectHeader = document.querySelector('.projectHeader');
 const scrollDown = document.querySelector('.scrollDown');
 const scrollDown2 = document.querySelector('.scrollDown2');
 const scrollUp = document.querySelector('.scrollUp');
-const nav1 = document.querySelector('#nav1');
-const nav2 = document.querySelector('#nav2');
-const nav3 = document.querySelector('#nav3');
-const navMenu = document.querySelector('.nav-menu');
-const verticalMenu = document.querySelector('.vertical-menu');
-const navClose = document.querySelector('.closeNav');
 
-const nav1Open = document.querySelector('#nav1.open');
-const nav2Open = document.querySelector('#nav2.open');
-const nav3Open = document.querySelector('#nav3.open');
-
-const navs = document.querySelectorAll('navI');
-
+const nav1 = document.querySelector('.nav1');
+const nav2 = document.querySelector('.nav2');
+const nav3 = document.querySelector('.nav3');
 //////////////////////////////////////////
 
 // Functions
 
-// Closes modal when you hit ESC
-window.onkeydown = function (event) {
-	if (event.keyCode == 27) {
-		modalBG.style.display = 'none';
-		modalBG2.style.display = 'none';
-	}
-};
-
-
-// Closes modal when you click the modal background
-window.onclick = function (e) {
-	if (e.target == modalBG || e.target == modalBG2) {
-		modalBG.style.display = 'none';
-		modalBG2.style.display = 'none';
-	}
-}
+nav2.addEventListener('click', () => {
+	nav2.classList.toggle('active');
+	nav1.classList.remove('active');
+})
 
 //////////////////////////////////////////
 
@@ -58,24 +37,12 @@ button1.addEventListener('click', () => {
 	skills.scrollIntoView(true);
 });
 
-nav1.addEventListener('click', () => {
-	window.scrollTo(0, 0);
-});
-
 button2.addEventListener('click', () => {
-	skills.scrollIntoView(true);
-});
-
-nav2.addEventListener('click', () => {
 	skills.scrollIntoView(true);
 });
 
 button3.addEventListener('click', () => {
 	window.scrollTo(0, 0);
-});
-
-nav3.addEventListener('click', () => {
-	projectHeader.scrollIntoView(true);
 });
 
 button1.addEventListener('mouseover', () => {
@@ -152,88 +119,21 @@ recipe.addEventListener('mouseout', () => {
 
 //////////////////////////////////////////
 
-// Modal Open & Close
-for (let i = 0; i < projects.length; i++) {
-	projects[i].addEventListener('click', () => {
-		modalBG.style.display = 'flex';
-	});
-}
 
-recipe.addEventListener('click', () => {
-	modalBG2.style.display = 'flex';
-});
-
-
-close.addEventListener('click', () => {
-	modalBG.style.display = 'none';
-});
-
-close2.addEventListener('click', () => {
-	modalBG2.style.display = 'none';
-});
-
-//////////////////////////////////////////
 
 // Side nav bar color changes on scroll
 $(window).scroll(function () {
 	var scrollPos = $(window).scrollTop(),
-		navH = $('.vertical-menu').height();
+		navH = $('.navbar').height();
 	$('.sections').each(function (i) {
 		var offT = $(this).offset().top;
 		if ((offT - scrollPos - navH) <= 0) {
-			$('.active').removeClass('active')
-			$('.vertical-menu a').eq(i).addClass('active')
+			$('.activeLink').removeClass('activeLink')
+			$('.navbar a').eq(i).addClass('activeLink')
 		}
 	})
 });
 
 
-//////////////////////////////////////////
-
-// Mobile Nav Bar
-
-const navBar = () => {
-	verticalMenu.classList.toggle("open");
-	nav1.classList.toggle("open");
-	nav2.classList.toggle("open");
-	nav3.classList.toggle("open");
-}
-
-navMenu.addEventListener('click', () => {
-	navBar();
-	navMenu.style.display = 'none';
-});
-
-navClose.addEventListener('click', () => {
-	navBar();
-	navMenu.style.display = 'block';
-});
-
-nav1.addEventListener('click', () => {
-	if (nav1.classList.contains("open")) {
-		navBar();
-		navMenu.style.display = 'block';
-	}
-})
-
-nav2.addEventListener('click', () => {
-	if (nav2.classList.contains("open")) {
-		navBar();
-		navMenu.style.display = 'block';
-	}
-})
-
-nav3.addEventListener('click', () => {
-	if (nav3.classList.contains("open")) {
-		navBar();
-		navMenu.style.display = 'block';
-	}
-})
-
-// for(let i = 0; i < navs.lenghth; i++) {
-// 	navs[i].addEventListener('click', () => {
-// 		navBar();
-// 	});
-// }
 
 //////////////////////////////////////////
